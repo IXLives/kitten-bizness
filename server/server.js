231d9a2e8aaf736881282ac47e154673b7212db8
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2020-07-28 13:46:09 -0400

    Server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 
 const publicPath = path.join(__dirname, '..', 'public')
 
-app.use(express.statuc(publicPath))
+app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
